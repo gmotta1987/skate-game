8f188b5c870fb411e2203be75b6297ed64fe709c
Gabriel Motta <gabriel.motta@valid.com>
2026-09-07 17:09:42 -0300

    feat: add SkateGame module build config

---
+++ Source/SkateGame/SkateGame.Build.cs
@@ -0,0 +1,18 @@
+using UnrealBuildTool;
+
+public class SkateGame : ModuleRules
+{
+    public SkateGame(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput"
+        });
+    }
+}
